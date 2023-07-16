71c96a990d788073ec6be23b59a5a3ba7dbc15bb
Your Name <you@example.com>
2023-07-16 13:59:43 +0300

    corsConfig

--- src/main/java/summerProject/demo/controllers/CharacterController.java
+++ src/main/java/summerProject/demo/controllers/CharacterController.java
@@ -8,6 +8,13 @@ import summerProject.demo.services.CharacterService;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
 @RestController
 @RequestMapping("/api/table/character")
 public class CharacterController {
